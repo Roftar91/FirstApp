c0abaf568fb516fb86e3642c0cfa23db5b16fdcc
resatou <satoray45@gmail.com>
2026-01-17 14:32:35 +0300

    добавлен функционал приложения

--- app/src/main/java/com/abramovich_mambetov/firstappam/MainActivity.kt
+++ app/src/main/java/com/abramovich_mambetov/firstappam/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.abramovich_mambetov.firstappam
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val resultTextView: TextView = findViewById(R.id.textInfo)
+        val button: Button = findViewById(R.id.buttonPull)
+        button.setOnClickListener { view ->
+            val number = (1..6).random()
+            resultTextView.text = number.toString()
         }
     }
 }
